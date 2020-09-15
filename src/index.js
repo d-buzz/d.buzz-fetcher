3f4168dcd710572c24bacdea00ab5980b0647638
Jhoii Rivera <lyereon.26@gmail.com>
2020-09-15 16:39:53 +0800

    error handling; replace http images to https

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ const express = require('express')
 const api = require('./api')
 const app = express()
 const cors = require('cors')
+const config = require('./config')
 
 app.use(cors())
-
+app.get('/', (req, res) => {
+    res.send({
+      status: 'Dbuzz metascraper is online',
+    })
+  })
+  
 app.use('/api/v1/meta', api)
-
-app.listen(3010, () => console.log('server running on port 3010'))
+app.listen(config.port, () => console.log('server running on port ',config.port))
